3f05d330947be3eb999a83181388af2dffb5a090
lijiwen <lijiwen@guazi.com>
2019-11-05 20:52:54 +0800

    初始化

---
+++ src/main/java/com/railwaymonitor/railway/monitor/RailwayMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.railwaymonitor.railway.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwayMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RailwayMonitorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/railwaymonitor/railway/monitor/RailwayMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.railwaymonitor.railway.monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailwayMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
